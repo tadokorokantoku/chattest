1380a65b37cd4e8afd9f261944b5e8f2388e5a41
井野　活輝 <Katsuki@Katuki-Book.local>
2017-06-04 18:48:54 +0900

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html:"hello,worlds"
+    render html:"hello,worlds!!!!"
   end
 end
